4dd8dac3e7c92b5c37695e8cac23e41d4c042a1e
jorn <jornschalkwijk@gmail.com>
2016-03-04 20:45:34 +0100

    changed database port for my mac

--- admin/model/db/db.go
+++ admin/model/db/db.go
@@ -2,8 +2,8 @@ package db
 
 //db_user,db_pass,@tcp(db_host:port)/db_name?charset=utf8
 var user string = "root"
-var password string= ""
+var password string= "root"
 var host string = "localhost"
 var name string = "nerdcms_db"
 
-var DB string = user+":"+password+"@tcp("+host+":3306)/"+name+"?charset=utf8"
+var DB string = user+":"+password+"@tcp("+host+":8889)/"+name+"?charset=utf8"
